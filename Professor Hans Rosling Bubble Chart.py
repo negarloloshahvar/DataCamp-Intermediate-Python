f9e286f8a7bad5fb36ab0f57242dae555cd8dc6a
Negar Loloshahvar <negarshahvar@gmail.com>
2022-10-21 11:42:10 +0100

    Update Professor Hans Rosling Bubble Chart.py

--- Professor Hans Rosling Bubble Chart.py
+++ Professor Hans Rosling Bubble Chart.py
@@ -1,7 +1,11 @@
 import pandas as pd
+from matplotlib import pyplot as plt
 
 life_exp = pd.read_csv('life_exp.csv')
 gdp_cap = pd.read_csv('gdp_cap.csv')
 
 print(life_exp.head())
 print(gdp_cap.head())
+
+plt.plot(life_exp, gdp_cap)
+plt.show()
