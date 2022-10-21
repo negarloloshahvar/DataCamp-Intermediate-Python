e629c5aca520cf09ba26822e1fe3cc12f510d70c
Negar Loloshahvar <negarshahvar@gmail.com>
2022-10-21 13:22:45 +0100

    update

--- Professor Hans Rosling Bubble Chart.py
+++ Professor Hans Rosling Bubble Chart.py
@@ -1,9 +1,13 @@
 import pandas as pd
 from matplotlib import pyplot as plt
+import numpy as np
 
 life_exp = pd.read_csv('life_exp.csv')
 life_exp1970 = pd.read_csv('life_exp1970.csv')
 gdp_cap = pd.read_csv('gdp_cap.csv')
+pop = pd.read_csv('pop.csv')
+col = pd.read_csv('col.csv')
+
 
 # plt.scatter(life_exp, gdp_cap)
 # plt.hist(life_exp, bins= 15)
@@ -14,12 +18,17 @@ gdp_cap = pd.read_csv('gdp_cap.csv')
 # plt.show()
 # plt.clf()
 
-plt.scatter(gdp_cap, life_exp)
+np_pop = np.array(pop)
+np_pop = np_pop * 2
+plt.scatter(gdp_cap, life_exp, s= np_pop, alpha= 0.8)
 
 plt.xscale('log')
 plt.xlabel('GDP per Capita [in USD]')
 plt.ylabel('Life Expectancy [in years]')
 plt.title('World Developement in 2007')
 plt.xticks([1000, 10000, 100000], ['1K', '10K', '100K'])
+
+plt.text(1550, 71, 'India')
+plt.text(5700, 80, 'China')
 
 plt.show()
