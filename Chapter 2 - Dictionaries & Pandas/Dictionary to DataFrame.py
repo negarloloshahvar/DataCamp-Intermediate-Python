3c8b6c79449665b1546a0572a6324891471b0b9b
Negar Loloshahvar <negarshahvar@gmail.com>
2022-10-23 17:44:18 +0100

    update name

---
+++ Chapter 2 - Dictionaries & Pandas/Dictionary to DataFrame.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+names = ['United States', 'Australia', 'Japan', 'India', 'Russia', 'Morocco', 'Egypt']
+dr = [True, False, False, False, True, True, True]
+cpc = [809, 731, 588, 18, 200, 70, 45]
+
+my_dict = {'country': names, 'drives_right': dr, 'cars_per_cap': cpc}
+
+cars = pd.DataFrame(my_dict)
+
+print(cars)
