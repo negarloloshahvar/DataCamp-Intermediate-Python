60383bcea675efeef784cdb80b4be9e5a4b8c41d
Negar Loloshahvar <negarshahvar@gmail.com>
2022-10-24 20:28:18 +0100

    update

---
+++ Chapter 5 -  Case Study: Hacker Statistics/Random Numbers.py
@@ -0,0 +1,8 @@
+import numpy as np
+#rand() from the random package generates a random number between 0 and 1.
+random_number = np.random.rand()
+print(random_number)
+
+#randint(a, b) from the random package generates a random integer between a and b.
+throw_a_dice = np.random.randint(1, 7)
+print(throw_a_dice)
---
+++ Chapter 5 -  Case Study: Hacker Statistics/The Empire State Bet.py
@@ -0,0 +1,8 @@
+# My friend and I are at the Empire State Building, and we are going to climb the
+# stairs. My climbing moves follows the following rules:
+# I throw a die 100 times. If I throw one or two, I will go one step down,
+# if it's three, four or five, I will go one step up, and If it's six, I throw the dice one
+# more time and walk up the resulting number. I cannot go below the number zero, and there is
+# a 0.1% chance of falling down the stairs and go back to number zero, and I have to start again.
+# I bet that with these rules in mind, I can reach 60 steps high. We want to calculate the chance
+# to win this bet, using simulation and Hacker Statistics.
